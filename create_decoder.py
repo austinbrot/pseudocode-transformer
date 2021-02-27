788fdfb4e772e1edae0daea8051ce3343c843e49
Kaan Ertas <kkaanertas@gmail.com>
2021-02-26 21:52:57 -0600

    it ain't much but it's honest work

--- create_decoder.py
+++ create_decoder.py
@@ -7,5 +7,7 @@ import sys, os, shutil, re, argparse, json
 config = BertConfig(vocab_size=5449)
 model = BertModel(config)
 config.is_decoder = True
+config.decoder_start_token_id = 1
+config.pad_token_id = 1
 
 model.save_pretrained('decoder-bert')
--- simple_model.py
+++ simple_model.py
@@ -13,32 +13,38 @@ model = transformers.EncoderDecoderModel.from_encoder_decoder_pretrained('bert-b
 text_tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
 code_tokenizer = Tokenizer.from_file("code_tokenizer.json")
 
-inp = text_tokenizer.encode('add 2 to i')
-input_ids = torch.tensor(inp).unsqueeze(0)  # Batch size 1
-decoder_input_ids = torch.tensor(code_tokenizer.encode('[PAD]').ids).unsqueeze(0)
+nlp = pipeline('text2text-generation',model=model,tokenizer=text_tokenizer)
+output = nlp('add 2 to i',return_tensors=True)
+print(output[0]['generated_token_ids'].numpy())
+print(code_tokenizer.decode(output[0]['generated_token_ids'].numpy()))
 
-outputs = model(input_ids=input_ids, decoder_input_ids=decoder_input_ids, return_dict=True)
+# inp = text_tokenizer.encode('add 2 to i')
+# input_ids = torch.tensor(inp).unsqueeze(0)  # Batch size 1
+# decoder_input_ids = torch.tensor(code_tokenizer.encode('[PAD]').ids).unsqueeze(0)
 
-encoded_sequence = (outputs.encoder_last_hidden_state,)
-lm_logits = outputs.logits
+# outputs = model(input_ids=input_ids, decoder_input_ids=decoder_input_ids, return_dict=True)
 
-# sample last token with highest prob
-next_decoder_input_ids = torch.argmax(lm_logits[:, -1:], axis=-1)
-print(code_tokenizer.decode(next_decoder_input_ids.numpy()[0]))
+# encoded_sequence = (outputs.encoder_last_hidden_state,)
+# lm_logits = outputs.logits
 
-# concat
-decoder_input_ids = torch.cat([decoder_input_ids, next_decoder_input_ids], axis=-1)
+# # sample last token with highest prob
+# next_decoder_input_ids = torch.argmax(lm_logits[:, -1:], axis=-1)
+# print(next_decoder_input_ids)
+# print(code_tokenizer.decode(next_decoder_input_ids.numpy()[0]))
+
+# # concat
+# decoder_input_ids = torch.cat([decoder_input_ids, next_decoder_input_ids], axis=-1)
 
 
-# STEP 2
-outputs = model(None, encoder_outputs=encoded_sequence, decoder_input_ids=decoder_input_ids, return_dict=True)
+# # STEP 2
+# outputs = model(None, encoder_outputs=encoded_sequence, decoder_input_ids=decoder_input_ids, return_dict=True)
 
-encoded_sequence = (outputs.encoder_last_hidden_state,)
-lm_logits = outputs.logits
+# encoded_sequence = (outputs.encoder_last_hidden_state,)
+# lm_logits = outputs.logits
 
-# sample last token with highest prob
-next_decoder_input_ids = torch.argmax(lm_logits[:, -1:], axis=-1)
-print(code_tokenizer.decode(next_decoder_input_ids.numpy()[0]))
+# # sample last token with highest prob
+# next_decoder_input_ids = torch.argmax(lm_logits[:, -1:], axis=-1)
+# print(code_tokenizer.decode(next_decoder_input_ids.numpy()[0]))
 
-# concat
-decoder_input_ids = torch.cat([decoder_input_ids, next_decoder_input_ids], axis=-1)
+# # concat
+# decoder_input_ids = torch.cat([decoder_input_ids, next_decoder_input_ids], axis=-1)
